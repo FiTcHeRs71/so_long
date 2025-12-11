fc41262274e58a53c431c9e7a642b28a19a16269
Frederic Ducrot <fducrot@c1r3s4.42lausanne.ch>
2025-12-11 15:40:04 +0100

    save

--- src/window.c
+++ src/window.c
@@ -15,7 +15,7 @@ void	handle_window(t_mlx *mlx)
 		// ft_error, free ?
 		close_window(mlx);
 	}
-	mlx->img.img_ptr = mlx_new_image(mlx, WIDTH, HEIGHT);
+	mlx->img.img_ptr = mlx_new_image(mlx->mlx_connect, WIDTH, HEIGHT);
 	mlx->img.pxl_ptr = mlx_get_data_addr(mlx->img.img_ptr, &mlx->img.bits, &mlx->img.len, &mlx->img.endlian);
 	mlx_put_image_to_window(mlx, mlx->mlx_window, mlx->img.img_ptr, 0, 0);
 	mlx_key_hook(mlx->mlx_window, handle_keyboard_input, mlx);
@@ -23,86 +23,11 @@ void	handle_window(t_mlx *mlx)
 	mlx_loop(mlx->mlx_connect);
 }
 
-int	handle_keyboard_input(int keycode, t_mlx *mlx)
-{
-	if (keycode == XK_Escape)
-	{
-		ft_printf("%d Esc pressed - closing window\n", keycode);
-		close_window(mlx);
-	}
-	else if (keycode == 65451)
-	{
-		resize_window(mlx, mlx->window.size_x +100, mlx->window.size_y + 100);
-	}
-	else if (keycode == 65453)
-	{
-		resize_window(mlx, mlx->window.size_x - 100, mlx->window.size_y -100);
-	}
-	else if (keycode == XK_r)
-	{
-		color_screen(mlx, 0xff0000);
-	}
-	ft_printf("%d touch press\n\n", keycode);
-	return (0);
-}
-
-void	color_screen(t_mlx *mlx, int color)
-{
-	int	x;
-	int	y;
-
-	y = 1;
-	x = 1;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			my_pixel_put(&mlx->img, x, y, color);
-			x++;
-			y++;
-		}
-		y++;
-	}
-
-}
-
-void	my_pixel_put(t_img *img, int x, int y, int color)
-{
-	int	offset;
-
-	offset = (img->len * y) + (x * (img->bits / 8));
-	*((unsigned int *)(offset + img->pxl_ptr)) = color;
-}
-
-void	resize_window(t_mlx *mlx, int new_width, int new_height)
-{
-	if (new_width < mlx->window.min_width)
-		new_width = mlx->window.min_width;
-	if (new_width > mlx->window.max_width)
-		new_width = mlx->window.max_width;
-	if (new_height < mlx->window.min_height)
-		new_height = mlx->window.min_height;
-	if (new_height > mlx->window.max_height)
-		new_height = mlx->window.max_height;
-	mlx_destroy_window(mlx->mlx_connect, mlx->mlx_window);
-	mlx->window.size_x = new_width;
-	mlx->window.size_y = new_height;
-	mlx->mlx_window = mlx_new_window(mlx->mlx_connect, new_width, new_height,
-			"So Long - Pokémon Style");
-	if (!mlx->mlx_window)
-	{
-		// ft_error, free ?
-		close_window(mlx);
-	}
-	mlx_key_hook(mlx->mlx_window, handle_keyboard_input, mlx);
-	mlx_hook(mlx->mlx_window, 17, 0, close_window, mlx);
-}
 
 int	close_window(t_mlx *mlx)
 {
 	mlx_destroy_window(mlx->mlx_connect, mlx->mlx_window);
 	mlx_destroy_display(mlx->mlx_connect);
 	free(mlx->mlx_connect); // a ranger autre part ?
-	exit(EXIT_SUCCESS);        // succes ?
+	exit(EXIT_SUCCESS);     // succes ?
 }
---
+++ trash.c
@@ -0,0 +1,83 @@
+else if (keycode == 65451)
+{
+	resize_window(mlx, mlx->window.size_x + 100, mlx->window.size_y + 100);
+}
+else if (keycode == 65453)
+{
+	resize_window(mlx, mlx->window.size_x - 100, mlx->window.size_y - 100);
+}
+else if (keycode == XK_r)
+{
+	color_screen(mlx, 0xff0000);
+}
+else if (keycode == XK_g)
+{
+	color_screen(mlx, 0xff00);
+}
+else if (keycode == XK_b)
+{
+	color_screen(mlx, 0xff);
+}
+
+int	handle_keyboard_input(int keycode, t_mlx *mlx)
+{
+	if (keycode == XK_Escape)
+	{
+		ft_printf("%d Esc pressed - closing window\n", keycode);
+		close_window(mlx);
+	}
+	ft_printf("%d touch press\n\n", keycode);
+	return (0);
+}
+
+void	color_screen(t_mlx *mlx, int color)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			my_pixel_put(&mlx->img, x, y, color);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(mlx->mlx_connect, mlx->mlx_window, mlx->img.img_ptr,
+		0, 0);
+}
+
+void	my_pixel_put(t_img *img, int x, int y, int color)
+{
+	int	offset;
+
+	offset = (img->len * y) + (x * (img->bits / 8));
+	*((unsigned int *)(offset + img->pxl_ptr)) = color;
+}
+
+void	resize_window(t_mlx *mlx, int new_width, int new_height)
+{
+	if (new_width < mlx->window.min_width)
+		new_width = mlx->window.min_width;
+	if (new_width > mlx->window.max_width)
+		new_width = mlx->window.max_width;
+	if (new_height < mlx->window.min_height)
+		new_height = mlx->window.min_height;
+	if (new_height > mlx->window.max_height)
+		new_height = mlx->window.max_height;
+	mlx_destroy_window(mlx->mlx_connect, mlx->mlx_window);
+	mlx->window.size_x = new_width;
+	mlx->window.size_y = new_height;
+	mlx->mlx_window = mlx_new_window(mlx->mlx_connect, new_width, new_height,
+			"So Long - Pokémon Style");
+	if (!mlx->mlx_window)
+	{
+		// ft_error, free ?
+		close_window(mlx);
+	}
+	mlx_key_hook(mlx->mlx_window, handle_keyboard_input, mlx);
+	mlx_hook(mlx->mlx_window, 17, 0, close_window, mlx);
+}
